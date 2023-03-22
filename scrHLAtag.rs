0b1045e576f9d92f84835750a742ee10aae95b20
Scott <scfurl@users.noreply.github.com>
2023-03-22 09:26:11 -0700

    create

---
+++ scrHLAtag.rs
@@ -0,0 +1,3 @@
+// scrHLA typing, alignment, : single cell rna-based HLA typing and alignment
+// this program takes a pacbio bam and performs alignment and prediction
+// single cell hla typing, alignment, and grouping - scrHLAtag
